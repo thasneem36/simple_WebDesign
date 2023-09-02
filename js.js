7632f8b04e5612d1fbb99df2820c8f64dba89476
thasneem36 <thasneem9mt@gmail.com>
2023-09-03 00:59:35 +0530

    Update js.js

--- js.js
+++ js.js
@@ -3,12 +3,6 @@ function redChange() {
     let soci_c_red = document.getElementById('soci')
     color_C_red.style.color = 'red' 
 
-    for(
-        i = 0;
-        i > soci_c_red;
-        i++
-    ){
-        soci_c_red.style.color = 'red';
-    }
+    
 
 }
