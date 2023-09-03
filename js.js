b6edc333e1d3b76a448dbc4865dae78cabf6231f
thasneem36 <thasneem9mt@gmail.com>
2023-09-03 13:42:51 +0530

    update index,js,style

--- js.js
+++ js.js
@@ -1,8 +1,69 @@
 function redChange() {
-    let color_C_red = document.getElementById('name')
-    let soci_c_red = document.getElementById('soci')
-    color_C_red.style.color = 'red' 
+    color_r = 'red';
+    //name change
+    let color_C_red = document.getElementById('name');
+    color_C_red.style.color = color_r;
 
-    
+    //social media change
+    let soci_c_red_1 = document.getElementById('soci1')
+    soci_c_red_1.style.color = color_r
 
+    let soci_c_red_2 = document.getElementById('soci2')
+    soci_c_red_2.style.color = color_r
+
+    let soci_c_red_3 = document.getElementById('soci3')
+    soci_c_red_3.style.color = color_r
+
+    let soci_c_red_4 = document.getElementById('soci4')
+    soci_c_red_4.style.color = color_r
+
+    let soci_c_red_5 = document.getElementById('soci5')
+    soci_c_red_5.style.color = color_r
+
+}
+
+function blueChange(){
+    color_r = 'blue';
+    //name change
+    let color_C_red = document.getElementById('name');
+    color_C_red.style.color = color_r;
+
+    //social media change
+    let soci_c_red_1 = document.getElementById('soci1')
+    soci_c_red_1.style.color = color_r;
+
+    let soci_c_red_2 = document.getElementById('soci2')
+    soci_c_red_2.style.color = color_r;
+
+    let soci_c_red_3 = document.getElementById('soci3')
+    soci_c_red_3.style.color = color_r;
+
+    let soci_c_red_4 = document.getElementById('soci4')
+    soci_c_red_4.style.color = color_r;
+
+    let soci_c_red_5 = document.getElementById('soci5')
+    soci_c_red_5.style.color = color_r;
+}
+
+function orangeChange(){
+    color_r = 'orange';
+    //name change
+    let color_C_red = document.getElementById('name');
+    color_C_red.style.color = color_r;
+
+    //social media change
+    let soci_c_red_1 = document.getElementById('soci1')
+    soci_c_red_1.style.color = color_r;
+
+    let soci_c_red_2 = document.getElementById('soci2')
+    soci_c_red_2.style.color = color_r;
+
+    let soci_c_red_3 = document.getElementById('soci3')
+    soci_c_red_3.style.color = color_r;
+
+    let soci_c_red_4 = document.getElementById('soci4')
+    soci_c_red_4.style.color = color_r;
+
+    let soci_c_red_5 = document.getElementById('soci5')
+    soci_c_red_5.style.color = color_r;
 }
