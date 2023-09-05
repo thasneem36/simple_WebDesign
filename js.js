846de16283271bc0aaad58de42355a1387742350
thasneem36 <thasneem9mt@gmail.com>
2023-09-05 16:15:59 +0530

    update

--- js.js
+++ js.js
@@ -29,16 +29,35 @@ function redChange() {
     let soci_c_red_6 = document.getElementById('soci6')
     soci_c_red_6.style.color = color_r
 
-    let copy_c = document.getElementById('copy')
-    copy_c.style.color = color_r; 
+    //round img color
+    let round1 = document.getElementById('round1')
+    round1.style.backgroundColor = color_r;
+    round1.style.boxShadow =  '2px 2px 10px red';
+    // round1.style.transition = '4s';
+
+    let round2 = document.getElementById('round2B')
+    round2.style.backgroundColor = color_r;
+    round2.style.boxShadow =  '2px 2px 10px red';
+
+    let round3 = document.getElementById('round3')
+    round3.style.backgroundColor = color_r;
+    round3.style.boxShadow =  '0 0 10px red';
+
+    let round4 = document.getElementById('round4')
+    round4.style.backgroundColor = color_r;
+    round4.style.boxShadow =  '0 0 10px red';
+
+    let round5 = document.getElementById('round5')
+    round5.style.backgroundColor = color_r;
+    round5.style.boxShadow =  '0 0 10px red';
 
 }
 
 function blueChange(){
-    color_r = 'blue';
+    color = 'blue';
     //name change
     let color_C_red = document.getElementById('name');
-    color_C_red.style.color = color_r;
+    color_C_red.style.color = color;
 
     //hello i am 
     let color_hello = document.getElementById('hello_c')
@@ -46,33 +65,53 @@ function blueChange(){
     color_hello.style.textShadow = '1px 1px 5px blue'
 
     //social media change
-    let soci_c_red_1 = document.getElementById('soci1')
-    soci_c_red_1.style.color = color_r;
+    let soci_c_blue_1 = document.getElementById('soci1')
+    soci_c_blue_1.style.color = color;
 
-    let soci_c_red_2 = document.getElementById('soci2')
-    soci_c_red_2.style.color = color_r;
+    let soci_c_blue_2 = document.getElementById('soci2')
+    soci_c_blue_2.style.color = color;
 
-    let soci_c_red_3 = document.getElementById('soci3')
-    soci_c_red_3.style.color = color_r;
+    let soci_c_blue_3 = document.getElementById('soci3')
+    soci_c_blue_3.style.color = color;
 
-    let soci_c_red_4 = document.getElementById('soci4')
-    soci_c_red_4.style.color = color_r;
+    let soci_c_blue_4 = document.getElementById('soci4')
+    soci_c_blue_4.style.color = color;
 
-    let soci_c_red_5 = document.getElementById('soci5')
-    soci_c_red_5.style.color = color_r;
+    let soci_c_blue_5 = document.getElementById('soci5')
+    soci_c_blue_5.style.color = color;
 
-    let soci_c_red_6 = document.getElementById('soci6')
-    soci_c_red_6.style.color = color_r;
+    let soci_c_blue_6 = document.getElementById('soci6')
+    soci_c_blue_6.style.color = color;
 
-    let copy_c = document.getElementById('copy')
-    copy_c.style.color = color_r;
+    //round img color
+    let round1 = document.getElementById('round1')
+    round1.style.backgroundColor = color;
+    round1.style.boxShadow =  '2px 2px 10px blue';
+    // round1.style.transition = '4s';
+
+    let round2 = document.getElementById('round2B')
+    round2.style.backgroundColor = color;
+    round2.style.boxShadow =  '2px 2px 10px blue';
+
+    let round3 = document.getElementById('round3')
+    round3.style.backgroundColor = color;
+    round3.style.boxShadow =  '0 0 10px blue';
+
+    let round4 = document.getElementById('round4')
+    round4.style.backgroundColor = color;
+    round4.style.boxShadow =  '0 0 10px blue';
+
+    let round5 = document.getElementById('round5')
+    round5.style.backgroundColor = color;
+    round5.style.boxShadow =  '0 0 10px blue';
+
 }
 
 function orangeChange(){
-    color_r = 'orange';
+    color = 'orange';
     //name change
-    let color_C_red = document.getElementById('name');
-    color_C_red.style.color = color_r;
+    let color_C_orange = document.getElementById('name');
+    color_C_orange.style.color = color;
 
     //hello i am 
     let color_hello = document.getElementById('hello_c')
@@ -80,25 +119,100 @@ function orangeChange(){
     color_hello.style.textShadow = '1px 1px 5px orange'
 
     //social media change
-    let soci_c_red_1 = document.getElementById('soci1')
-    soci_c_red_1.style.color = color_r;
+    let soci_c_orange_1 = document.getElementById('soci1')
+    soci_c_orange_1.style.color = color;
 
-    let soci_c_red_2 = document.getElementById('soci2')
-    soci_c_red_2.style.color = color_r;
+    let soci_c_orange_2 = document.getElementById('soci2')
+    soci_c_orange_2.style.color = color;
 
-    let soci_c_red_3 = document.getElementById('soci3')
-    soci_c_red_3.style.color = color_r;
+    let soci_c_orange_3 = document.getElementById('soci3')
+    soci_c_orange_3.style.color = color;
 
-    let soci_c_red_4 = document.getElementById('soci4')
-    soci_c_red_4.style.color = color_r;
+    let soci_c_orange_4 = document.getElementById('soci4')
+    soci_c_orange_4.style.color = color;
 
-    let soci_c_red_5 = document.getElementById('soci5')
-    soci_c_red_5.style.color = color_r;
+    let soci_c_orange_5 = document.getElementById('soci5')
+    soci_c_orange_5.style.color = color;
 
-    let soci_c_red_6 = document.getElementById('soci6')
-    soci_c_red_6.style.color = color_r;
+    let soci_c_orange_6 = document.getElementById('soci6')
+    soci_c_orange_6.style.color = color;
 
-    let copy_c = document.getElementById('copy')
-    copy_c.style.color = color_r;
+
+    //round img color
+    let round1 = document.getElementById('round1')
+    round1.style.backgroundColor = color;
+    round1.style.boxShadow =  '2px 2px 10px orange';
+    // round1.style.transition = '4s';
+
+    let round2 = document.getElementById('round2B')
+    round2.style.backgroundColor = color;
+    round2.style.boxShadow =  '2px 2px 10px orange';
+
+    let round3 = document.getElementById('round3')
+    round3.style.backgroundColor = color;
+    round3.style.boxShadow =  '0 0 10px orange';
+
+    let round4 = document.getElementById('round4')
+    round4.style.backgroundColor = color;
+    round4.style.boxShadow =  '0 0 10px orange';
+
+    let round5 = document.getElementById('round5')
+    round5.style.backgroundColor = color;
+    round5.style.boxShadow =  '0 0 10px orange';
 }
+
+function greedChange(){
+    color = 'green';
+    //name change
+    let color_C_green = document.getElementById('name');
+    color_C_green.style.color = color;
+
+    //hello i am 
+    let color_hello = document.getElementById('hello_c')
+    color_hello.style.color = color;
+    color_hello.style.textShadow = '1px 1px 5px green'
+
+    //social media change
+    let soci_c_green_1 = document.getElementById('soci1')
+    soci_c_green_1.style.color = color;
+
+    let soci_c_green_2 = document.getElementById('soci2')
+    soci_c_green_2.style.color = color;
+
+    let soci_c_green_3 = document.getElementById('soci3')
+    soci_c_green_3.style.color = color;
+
+    let soci_c_green_4 = document.getElementById('soci4')
+    soci_c_green_4.style.color = color;
+
+    let soci_c_green_5 = document.getElementById('soci5')
+    soci_c_green_5.style.color = color;
+
+    let soci_c_green_6 = document.getElementById('soci6')
+    soci_c_green_6.style.color = color;
+
+
+    //round img color
+    let round1 = document.getElementById('round1')
+    round1.style.backgroundColor = color;
+    round1.style.boxShadow =  '2px 2px 10px green';
+    // round1.style.transition = '4s';
+
+    let round2 = document.getElementById('round2B')
+    round2.style.backgroundColor = color;
+    round2.style.boxShadow =  '2px 2px 10px green';
+
+    let round3 = document.getElementById('round3')
+    round3.style.backgroundColor = color;
+    round3.style.boxShadow =  '0 0 10px green';
+
+    let round4 = document.getElementById('round4')
+    round4.style.backgroundColor = color;
+    round4.style.boxShadow =  '0 0 10px green';
+
+    let round5 = document.getElementById('round5')
+    round5.style.backgroundColor = color;
+    round5.style.boxShadow =  '0 0 10px green';
+}
+
 
